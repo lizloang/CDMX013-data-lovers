59dceb3a626b0e24cbc84583db26da1c987b88bb
vanesadeneb <vanessa_diesver@hotmail.com>
2022-08-01 09:41:34 -0700

    Show the character's personal information

--- src/main.js
+++ src/main.js
@@ -8,12 +8,21 @@ for (let i = 0; i < data.results.length; i++) {
   let img = document.createElement("img");
   img.src = data.results[i].image;
   let figcaption = document.createElement("figcaption");
-  figcaption.innerHTML = "caption";
 
   let cardFigure = document.getElementById("root").appendChild(figure);
-
-  let cardImage = cardFigure.appendChild(img);
-  cardFigure.appendChild(figcaption);
+  cardFigure.appendChild(img);
+  let caption = cardFigure.appendChild(figcaption);
+  let p = caption.appendChild(document.createElement("p"));
+  p.innerHTML = data.results[i].name;
+  let aliveSpan = caption.appendChild(document.createElement("span"));
+  aliveSpan.innerHTML = data.results[i].status;
+  let specieSpan = caption.appendChild(document.createElement("span"));
+  specieSpan.innerHTML = data.results[i].species;
+  let genderSpan = caption.appendChild(document.createElement("span"));
+  genderSpan.innerHTML = data.results[i].gender;
+  let button = caption.appendChild(document.createElement("button"));
+  button.innerHTML = "show more";
 
   console.log("entro");
+
 }
