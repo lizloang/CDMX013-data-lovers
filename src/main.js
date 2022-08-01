d8e502174b0a0b54252bf6a58e767a07afbd2934
lizloang <liz.loang@gmail.com>
2022-08-01 11:01:51 -0500

    caption in figures

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 //import { example } from './data.js';
 // import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
-import data from './data/rickandmorty/rickandmorty.js';
+import data from "./data/rickandmorty/rickandmorty.js";
 
-console.log(data);
-for(let i=0; i < data.results.length; i++){
-    let figure = document.createElement("figure");
-    let img = document.createElement("img");
-    let figcaption = document.createElement("figcaption");
-    let p = document.createElement("p");
-    document.getElementById("root").appendChild(figure);
-    document.querySelector("figure").appendChild(img).src = data.results[i].image;
-    document.body.appendChild(p);
+for (let i = 0; i < data.results.length; i++) {
+  let figure = document.createElement("figure");
+  let img = document.createElement("img");
+  img.src = data.results[i].image;
+  let figcaption = document.createElement("figcaption");
+  figcaption.innerHTML = "caption";
+
+  let cardFigure = document.getElementById("root").appendChild(figure);
+
+  let cardImage = cardFigure.appendChild(img);
+  cardFigure.appendChild(figcaption);
+
+  console.log("entro");
 }
