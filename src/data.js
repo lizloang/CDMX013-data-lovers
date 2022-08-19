9f6fa5c1445c3aac3e175117d4114aa063794c45
vanesadeneb <vanessa_diesver@hotmail.com>
2022-08-19 09:29:49 -0700

    change name variable for searchInput function

--- src/data.js
+++ src/data.js
@@ -22,9 +22,7 @@ export const sortData = (data, sortOrder) => {
 };
 
 export const searchInput = (data, value) => {
-  console.log("value searchInput: " + value.toLowerCase());
-  const buscador = data.filter(element => element.name.toLowerCase().includes(value.toLowerCase()));
-  console.log("buscador: " + buscador);
-  return buscador;
+  const searcher = data.filter(element => element.name.toLowerCase().includes(value.toLowerCase()));
+  return searcher;
 };
 
